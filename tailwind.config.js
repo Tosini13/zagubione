d49679dc4449d3ac3ae44b2f176705adefe71967
Tosini13 <jbartos13@gmail.com>
2023-10-26 23:10:00 +0200

    fix: configure tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#292C45",
+          light: "#D6E6FE",
+          on: "#D6E6FE",
+          variant: {
+            DEFAULT: "#4992FF",
+            on: "#FFF",
+          },
+        },
+        secondary: { label: "#A3A8D2", tab: "#00BF13" },
+        background: {
+          DEFAULT: "#fff",
+          on: "#000",
+        },
+      },
+    },
   },
   plugins: [],
 };
