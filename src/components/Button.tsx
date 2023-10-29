f99f16ca7ec8cc42a25a7567621b43284ee85289
Tosini13 <jbartos13@gmail.com>
2023-10-29 14:33:24 +0100

    fix: button to take icon prop as string or react element

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,38 +1,43 @@
 import React, {
   ButtonHTMLAttributes,
   InputHTMLAttributes,
+  ReactElement,
   ReactNode,
 } from "react";
 import { twMerge } from "tailwind-merge";
 import { getIconSrc } from "../utils";
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
-  iconSrc?: string;
+  icon?: string | ReactElement;
+  iconClassName?: string;
 };
 
 const Button: React.FC<ButtonProps> = ({
-  iconSrc,
+  icon,
   className,
   children,
+  iconClassName,
   ...rest
 }) => {
   return (
     <button
       className={twMerge(
-        "flex items-center gap-x-2 rounded-xl font-bold bg-primary-variant text-primary-variant-on hover:brightness-110 py-4 px-8 transition-all",
+        "flex items-center justify-center gap-x-2 rounded-xl font-bold bg-primary-variant text-primary-variant-on hover:brightness-110 py-4 px-8 transition-all",
         className
       )}
       {...rest}
     >
-      {iconSrc && (
+      {typeof icon === "string" ? (
         <img
           loading="lazy"
-          srcSet={iconSrc}
+          srcSet={icon}
           alt="button icon"
-          className="aspect-square h-4"
+          className={twMerge("aspect-square h-4", iconClassName)}
         />
-      )}
-      {children}
+      ) : React.isValidElement(icon) ? (
+        icon
+      ) : null}
+      <span>{children}</span>
     </button>
   );
 };
