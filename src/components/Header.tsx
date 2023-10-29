c0c6c6f79357e9699436fb6865b3a5d684e9a773
Tosini13 <jbartos13@gmail.com>
2023-10-29 14:32:16 +0100

    feat: implement links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,9 @@
+import { Link } from "react-router-dom";
 import { getIconSrc } from "../utils";
 
 export default function Header() {
   return (
-    <nav className="justify-between items-start flex max-md:flex-wrap py-4 max-w-screen-xl mx-auto">
+    <nav className="justify-between items-start flex max-md:flex-wrap py-4 max-w-screen-xl mx-auto h-header">
       <div className="items-center text-base flex gap-10 my-auto px-5 max-md:flex-wrap">
         <h1 className="font-bold leading-tight flex items-center">
           <img
@@ -14,29 +15,29 @@ export default function Header() {
           <span className="text-primary-variant">.com</span>
         </h1>
         <div className="items-start self-stretch flex grow shrink-0 basis-auto gap-x-10 max-md:flex-wrap">
-          <a className="link" href="/home">
+          <Link className="link" to="/">
             Strona główna
-          </a>
-          <a className="link" href="/search">
+          </Link>
+          <Link className="link" to="/search">
             Wyszukiwanie
-          </a>
-          <a className="link" href="/how-do-we-work">
+          </Link>
+          <Link className="link" to="/how-do-we-work">
             Jak działamy?
-          </a>
-          <a className="link" href="/reviews">
+          </Link>
+          <Link className="link" to="/reviews">
             Opinie
-          </a>
+          </Link>
         </div>
       </div>
       <div className="items-center self-stretch flex gap-5">
         <div className="items-start flex gap-2 max-md:justify-center">
-          <a href="/log-in" className="link">
+          <Link to="/login" className="link">
             Log In
-          </a>
+          </Link>
           <span className="text-base"> / </span>
-          <a href="/sign-up" className="link">
+          <Link to="/sign-up" className="link">
             Sign Up
-          </a>
+          </Link>
         </div>
         <button className="button button-variant">Zgłoś</button>
       </div>
