b87a02eb1c959bc971a92916d200e20a2cc42e32
Tosini13 <jbartos13@gmail.com>
2023-10-30 01:54:42 +0100

    feat: add show password

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,26 +1,80 @@
-import React, { InputHTMLAttributes, ReactNode, forwardRef } from "react";
+import React, {
+  InputHTMLAttributes,
+  ReactElement,
+  ReactNode,
+  forwardRef,
+  useState,
+} from "react";
 import { twMerge } from "tailwind-merge";
+import { EyeIcon, EyeSlashIcon } from "@heroicons/react/20/solid";
 
-type InputProps = InputHTMLAttributes<HTMLInputElement> & {
-  icon?: ReactNode;
+type BaseInputProps = InputHTMLAttributes<HTMLInputElement> & {
+  icon?: string | ReactElement;
+  iconClassName?: string;
   inputClassName?: string;
 };
 
-const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ icon, className, inputClassName, ...rest }: InputProps, ref) => {
-    return (
-      <div className={twMerge("flex items-center rounded-lg", className)}>
-        {icon}
-        <input
-          ref={ref}
-          {...rest}
-          className={twMerge(
-            "w-full px-2 py-2 focus:outline-none !bg-transparent",
-            inputClassName
-          )}
+const BaseInput = forwardRef<HTMLInputElement, BaseInputProps>(
+  (
+    { icon, className, inputClassName, iconClassName, ...rest }: BaseInputProps,
+    ref
+  ) => (
+    <div className={twMerge("flex items-center rounded-lg", className)}>
+      {typeof icon === "string" ? (
+        <img
+          loading="lazy"
+          srcSet={icon}
+          alt="button icon"
+          className={twMerge("aspect-square h-4", iconClassName)}
         />
-      </div>
+      ) : React.isValidElement(icon) ? (
+        icon
+      ) : null}
+      <input
+        ref={ref}
+        {...rest}
+        className={twMerge(
+          "w-full px-4 py-2 focus:outline-none !bg-transparent",
+          inputClassName
+        )}
+      />
+    </div>
+  )
+);
+
+const InputPassword = forwardRef<HTMLInputElement, BaseInputProps>(
+  (props: BaseInputProps, ref) => {
+    const [visibility, setVisibility] = useState<"showed" | "hidden">("hidden");
+    return (
+      <BaseInput
+        data-testid="input_password"
+        inputClassName="order-1"
+        icon={
+          visibility === "showed" ? (
+            <EyeSlashIcon
+              className="aspect-square h-5 mx-4 order-2"
+              onClick={() => setVisibility("hidden")}
+            />
+          ) : (
+            <EyeIcon
+              className="aspect-square h-5 mx-4 order-2"
+              onClick={() => setVisibility("showed")}
+            />
+          )
+        }
+        {...props}
+        type={visibility === "showed" ? "text" : "password"}
+        ref={ref}
+      />
     );
+  }
+);
+
+const Input = forwardRef<HTMLInputElement, BaseInputProps>(
+  (props: BaseInputProps, ref) => {
+    if (props.type === "password")
+      return <InputPassword ref={ref} {...props} />;
+    return <BaseInput ref={ref} {...props} />;
   }
 );
 
