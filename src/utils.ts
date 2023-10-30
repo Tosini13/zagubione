a73dfed91bc12acde0044cf96a672d9bb8185d74
Tosini13 <jbartos13@gmail.com>
2023-10-30 09:00:06 +0100

    feat: add util for db files

--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,9 @@
 export const getIconSrc = (name: string) => `/icons/${name}`;
+
+export const getDBFileSrc = (
+  collection: string,
+  recordId: string,
+  filename: string,
+  thumb?: string
+) =>
+  `${process.env.REACT_APP_POCKET_BASE_SERVER_URL}/api/files/${collection}/${recordId}/${filename}?thumb=${thumb}`;
